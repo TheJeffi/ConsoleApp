cae472e84b21938b86f56878996b83547584077f
TheJeffi <Thejeffi@yandex.ru>
2022-06-19 02:13:00 +0300

    Add files via upload

---
+++ ConsoleAppTask23-24/ConsoleAppTask23-24/StraightTriangularPrism.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTask22._2
+{
+    internal class StraightTriangularPrism : Cylinder
+    {
+        protected PointOnPlane pointThree;
+        public PointOnPlane PointThree { get { return pointThree; } set { pointThree = value; } }
+
+        public override double GetFootSquare()
+        {
+            double p = GetFootPerimeter() / 2;
+            return Math.Round(Math.Sqrt(p * (p - pointOne.GetDistanceTo(PointTwo)) * (p - pointTwo.GetDistanceTo(PointThree)) * (p - pointThree.GetDistanceTo(pointTwo))), 2);
+        }
+
+        public override double GetFootPerimeter()
+        {
+            return Math.Round(pointOne.GetDistanceTo(PointTwo) + pointTwo.GetDistanceTo(PointThree) + pointThree.GetDistanceTo(pointTwo), 2);
+        }
+        public  StraightTriangularPrism():base()
+        {
+            pointThree = new PointOnPlane();
+        }
+    }
+}
