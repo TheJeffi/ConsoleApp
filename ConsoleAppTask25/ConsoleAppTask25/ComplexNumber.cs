3ff440a01abbc07a1c8a9285eec560f630a6dd0e
TheJeffi <Thejeffi@yandex.ru>
2022-06-19 02:13:43 +0300

    Add files via upload

---
+++ ConsoleAppTask25/ConsoleAppTask25/ComplexNumber.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleAppTask25
+{
+    internal class ComplexNumber
+    {
+        private double _imaginaryNumber;
+        private double _realNumber;
+
+        public double ImaginaryNumber { get { return _imaginaryNumber; } set { _imaginaryNumber = value; } }
+        public double RealNumber { get { return _realNumber; } set { _realNumber = value; } }
+
+        public string ComplexNumberValue { get { return $"{_realNumber}+i{_imaginaryNumber}"; } 
+            set
+            {
+                Regex stringParse = new Regex(@"^(\-)?(\d+)\s?(\+|\-)\s?i(\d+)$");
+                Match match = stringParse.Match(value);
+                if (match.Groups.Count >= 3)
+                {   
+                    RealNumber = GetParseNumber(match.Groups[1].Value, match.Groups[2].Value);
+                    ImaginaryNumber = GetParseNumber(match.Groups[3].Value, match.Groups[4].Value);
+                }
+            } 
+        }
+
+        public ComplexNumber() : this(0, 0) { }
+        public ComplexNumber(double _realNumber,double _imaginaryNumber)
+        {
+            ImaginaryNumber = _imaginaryNumber;
+            RealNumber = _realNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Действительная часть числа {_realNumber}, мнимая часть числа {_imaginaryNumber}, комплексное число {GetCompleteNumber()}, модуль числа {GetComplexNumberModule()}";
+        }
+
+        public double GetComplexNumberModule()
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(RealNumber,2)+ Math.Pow(ImaginaryNumber, 2)),3);
+        }
+
+        private double GetParseNumber(string symbol, string number)
+        {
+            return (symbol == "-")?double.Parse(number)*(-1) : double.Parse(number);
+        }
+        private string GetCompleteNumber()
+        {
+            return ($"{_realNumber}{(_imaginaryNumber<0?"-":"+")}i{Math.Abs(_imaginaryNumber)}");
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is ComplexNumber number ? _realNumber == number.RealNumber && _imaginaryNumber == number.ImaginaryNumber : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public ComplexNumber Sum(ComplexNumber number)
+        {
+           double realNumber = number.RealNumber + _realNumber;
+           double imaginaryNumber = number.ImaginaryNumber + _imaginaryNumber;
+           return new ComplexNumber(realNumber, imaginaryNumber);
+        }
+
+        public ComplexNumber Subtract(ComplexNumber number)
+        {
+            double realNumber = number.RealNumber - _realNumber;
+            double imaginaryNumber = number.ImaginaryNumber - _imaginaryNumber;
+            return new ComplexNumber(realNumber, imaginaryNumber);
+        }
+
+        public ComplexNumber Multiply(ComplexNumber number)
+        {
+            double realNumber = _realNumber*number.RealNumber - _imaginaryNumber*number.ImaginaryNumber;
+            double imaginaryNumber = _realNumber * number.ImaginaryNumber +_imaginaryNumber * number.RealNumber;
+            return new ComplexNumber(realNumber, imaginaryNumber);
+        }
+        public ComplexNumber Divide(ComplexNumber number)
+        {   
+            double realNumber = (_realNumber*number.RealNumber + _imaginaryNumber*number.ImaginaryNumber)/(Math.Pow(number.RealNumber,2)+ Math.Pow(number.ImaginaryNumber,2));
+            double imaginaryNumber = (_imaginaryNumber * number.RealNumber - _realNumber * number.ImaginaryNumber) / (Math.Pow(number.RealNumber, 2) + Math.Pow(number.ImaginaryNumber, 2));
+            return new ComplexNumber(realNumber, imaginaryNumber);
+        }
+
+    }
+}
---
+++ ConsoleAppTask25/ConsoleAppTask25/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleAppTask25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.PartOne();
+            program.PartTwo();
+        }
+
+        public void PartOne()
+        {
+            ComplexNumber number1 = new ComplexNumber();
+            Input(number1);
+            
+            ComplexNumber number2 = new ComplexNumber(4,3);
+
+            ComplexNumber number3 = new ComplexNumber(_realNumber: 2, _imaginaryNumber: -2);
+
+            number1.RealNumber *= 3;
+            number2.ImaginaryNumber -= 2;
+            number3.RealNumber += 10;
+            number3.ImaginaryNumber *= 10;
+
+            Console.WriteLine($"{number1}, Модуль комплексного числа = {number1.GetComplexNumberModule()}");
+
+            Console.WriteLine($"{number2}, Модуль комплексного  числа = {number2.GetComplexNumberModule()}");
+
+            Console.WriteLine($"{number3}, Модуль комплексного  числа = {number3.GetComplexNumberModule()}");
+
+            ComplexNumber number4 = new ComplexNumber(_realNumber: 2, _imaginaryNumber: 2);
+
+            ComplexNumber number5 = new ComplexNumber(_realNumber: 2, _imaginaryNumber: 2);
+
+            ComplexNumber number6 = new ComplexNumber(_realNumber: 2, _imaginaryNumber: -2);
+
+            Console.WriteLine(number6.Equals(number5));
+
+            Console.WriteLine(number4.Equals(number5));
+        }
+
+        public void PartTwo()
+        {
+            ComplexNumber number = new ComplexNumber(-3, 4);
+            ComplexNumber number2 = new ComplexNumber();
+            Input(number2);
+
+            Console.WriteLine($"Сумма первого и второго комплексных чисел = {number.Sum(number2)}");
+            Console.WriteLine($"Разность первого и второго комплексных чисел = {number.Subtract(number2)}");
+            Console.WriteLine($"Произведение первого и второго комплексных чисел = {number.Multiply(number2)}");
+            Console.WriteLine($"Частное первого и второго комплексных чисел = {number.Divide(number2)}");
+            Console.WriteLine($"Сумма произведения первого и второго чисел и их частного = {number.Multiply(number2).Sum(number.Divide(number2))}");
+
+
+        }
+        public void Input(ComplexNumber number)
+        {
+            Console.WriteLine("Введите комплексное число: (a+ib)");
+            number.ComplexNumberValue = Console.ReadLine()!.Trim();
+        }
+    }
+}
